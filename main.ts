a5a83d64472f18260ba1a207e0511f85b63216b0
maxflores1 <118218453+maxflores1@users.noreply.github.com>
2022-12-09 12:52:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,40 @@
-
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 5 5 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . b b b b b 5 5 5 5 5 5 5 b . . 
+    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+    . . b d 5 5 b 1 f f 5 4 4 c . . 
+    b b d b 5 5 5 d f b 4 4 4 4 b . 
+    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+    c d d d c c b 5 5 5 5 5 5 5 b . 
+    c b d d d d d 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+let shark = sprites.create(img`
+    .............ccfff..............
+    ............cddbbf..............
+    ...........cddbbf...............
+    ..........fccbbcf............ccc
+    ....ffffffccccccff.........ccbbc
+    ..ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .fb11111111bbbbbbcccccccccbccf..
+    ..fccc33cc11bbbbccccccccfffbbcf.
+    ...fc131c111bbbcccccbdbc...fbbf.
+    ....f33c111cbbbfdddddcc.....fbbf
+    .....ff1111fbdbbfddcc........fff
+    .......cccccfbdbbfc.............
+    .............fffff..............
+    `, SpriteKind.Player)
+shark.setPosition(0, randint(scene.screenWidth(), scene.screenHeight()))
+shark.follow(mySprite, 40)
